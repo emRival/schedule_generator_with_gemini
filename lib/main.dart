a3666ce8c1eb1b0c88520441fb022ae8dfe62dfd
RIVAL Mac's <macbook@RIVAL.local>
2025-03-13 14:09:08 +0700

    add: device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:schedule_generator/ui/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-  ));
+  runApp(DevicePreview(
+      // jika di web, ubah menjadi true
+      enabled: true,
+      defaultDevice: Devices.ios.iPhone13ProMax,
+      devices: [Devices.ios.iPhone13ProMax],
+      builder: (context) => MaterialApp(
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            debugShowCheckedModeBanner: false,
+            home: HomeScreen(),
+          )));
 }
